1bf4b8e577e5a294faedf415af4b654fc8f6b365
ANIK-MOHANTA24 <mohontoa050@gmail.com>
2024-02-20 22:42:43 +0600

    added seat position & seat count

--- scripts/e-ticket.js
+++ scripts/e-ticket.js
@@ -1,4 +1,18 @@
-function handelSelect() {
-  console.log('Select korci');
-  // document.getElementById();
+// function handelSelect() {
+//   console.log('Select korci');
+//   // document.getElementById();
+// }
+// *********step-1: get a array for all set btn************************
+const kbdSeatButton = document.getElementsByClassName('kbd-lg');
+// console.log(kbdSeatButton);
+
+let count = 0;
+for (const btn of kbdSeatButton) {
+  // console.log(btn);
+  btn.addEventListener('click', function () {
+    // console.log('boss sclect korci');
+    count = count + 1;
+    console.log(count, 'Yeaa! seat sclect korci');
+    document.getElementById('card-count').innerText = count;
+  });
 }
