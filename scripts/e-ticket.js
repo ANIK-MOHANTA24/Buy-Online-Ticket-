0d68e4734c575f2b7e7f076c160d123b71d05a66
ANIK-MOHANTA24 <mohontoa050@gmail.com>
2024-02-20 23:29:25 +0600

    added available seat number

--- scripts/e-ticket.js
+++ scripts/e-ticket.js
@@ -7,12 +7,18 @@ const kbdSeatButton = document.getElementsByClassName('kbd-lg');
 // console.log(kbdSeatButton);
 
 let count = 0;
+let available = 40;
 for (const btn of kbdSeatButton) {
   // console.log(btn);
+
   btn.addEventListener('click', function () {
     // console.log('boss sclect korci');
     count = count + 1;
     console.log(count, 'Yeaa! seat sclect korci');
+    //*******for count seat ************/
     document.getElementById('card-count').innerText = count;
+    //*******for available seat **********/
+    available = available - 1;
+    document.getElementById('available-count').innerText = available;
   });
 }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,1 +1,7 @@
 // console.log('update code');
+
+//for count seat
+// function handleButton(elementId) {
+//   const evtentIdBtn = document.getElementById(elementId);
+//   const enentId = evtentIdBtn.innerText();
+// }
